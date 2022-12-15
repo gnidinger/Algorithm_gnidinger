6ccaf82b728d39add7146765a3ad73c776b9e78a
gnidinger <gcnb87@gmail.com>
2022-12-15 11:22:20 +0900

    [Silver V] Title: 셀프 넘버, Time: 2008 ms, Memory: 300728 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        for (int i = 1; i <= 10000; i++) {
+            list1.add(i);
+        }
+
+        for (int i = 1; i <= 10000; i++) {
+            list2.add(i +
+                    i % 10 +
+                    (i % 100) / 10 +
+                    (i % 1000) / 100 +
+                    (i % 10000) / 1000);
+        }
+
+        List<Integer> result = list1.stream()
+                .filter(a -> list2.stream()
+                        .distinct()
+                        .filter(b -> b <= 10000)
+                        .noneMatch(Predicate.isEqual(a)))
+                .sorted()
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < result.size(); i++) {
+            System.out.println(result.get(i));
+        }
+    }
+}
