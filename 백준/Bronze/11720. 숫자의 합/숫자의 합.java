ea3414806650e91ab49eabe2bafc1ffc118d88d3
gnidinger <gcnb87@gmail.com>
2022-12-16 16:56:56 +0900

    Update 숫자의 합.java

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -1,22 +1,13 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Prob11720Stream {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        String x = sc.next();
 
-        List<String> list = new ArrayList<>();
-
-        list.add(x);
-
-        int result = list.stream()
-                .flatMap(a -> Arrays.stream(a.split("")))
-                .mapToInt(Integer::parseInt)
+        int result = sc.next().chars()
+                .map(a -> a - 48)
                 .sum();
 
         System.out.println(result);
