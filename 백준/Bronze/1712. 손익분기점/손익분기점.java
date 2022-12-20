7113168dae1c846cf83b566410fd6df5f7b96abc
gnidinger <gcnb87@gmail.com>
2022-12-20 11:11:03 +0900

    [Bronze II] Title: 손익분기점, Time: 216 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (b >= c) System.out.println(-1);
+        else {
+            
+            int point = a / (c - b) + 1;
+            
+            System.out.println(point);
+        }
+    }
+}
