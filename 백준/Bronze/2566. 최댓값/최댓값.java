44b996f81a9d3e884046dc539633210982d9282a
gnidinger <gcnb87@gmail.com>
2022-12-25 14:53:28 +0900

    [Bronze III] Title: 최댓값, Time: 240 ms, Memory: 18544 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 81; i++) {
+            list.add(sc.nextInt());
+        }
+
+        int max = list.stream()
+                .mapToInt(Integer::intValue)
+                .max().getAsInt();
+
+        int idx = list.indexOf(max);
+
+        System.out.println(max);
+        System.out.println(idx / 9 + 1 + " " + (idx % 9 + 1));
+    }
+}
