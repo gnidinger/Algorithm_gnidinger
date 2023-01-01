95c85586c329f86dcd5d6a33f45cb53b71c75fd2
gnidinger <gcnb87@gmail.com>
2023-01-02 00:13:19 +0900

    [Silver II] Title: 좌표 압축, Time: 2456 ms, Memory: 367420 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        List<Integer> sortedList = list.stream()
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
+
+        int num = 0;
+
+        for (int i : sortedList) {
+            if (!map.containsKey(i)) map.put(i,num);
+            num++;
+        }
+
+        for (int i : list) {
+            bw.write(map.get(i) + " ");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
