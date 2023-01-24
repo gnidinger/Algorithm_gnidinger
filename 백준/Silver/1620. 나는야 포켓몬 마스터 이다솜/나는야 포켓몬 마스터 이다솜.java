f1738b8ec4e5d9905c66581a3d9e7272faab44f5
gnidinger <gcnb87@gmail.com>
2023-01-24 22:49:46 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 584 ms, Memory: 61596 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Map<String, String> map = new HashMap<>();
+        Map<String, String> reverseMap = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            map.put(String.valueOf(i + 1), str);
+            reverseMap.put(str, String.valueOf(i + 1));
+        }
+
+        for (int i = 0; i < m; i++) {
+            String tmp = br.readLine();
+            if (map.containsKey(tmp)) sb.append(map.get(tmp)).append('\n');
+            else {
+                sb.append(reverseMap.get(tmp)).append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+}
