913c9c2d409678d22b53299f5dd5a097dadbb595
gnidinger <gcnb87@gmail.com>
2023-01-29 16:19:17 +0900

    [Silver IV] Title: 듣보잡, Time: 452 ms, Memory: 29012 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Set<String> setN = new TreeSet<>();
+        Set<String> setM = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            setN.add(br.readLine());
+        }
+
+        for (int i = 0; i < m; i++) {
+            setM.add(br.readLine());
+        }
+
+        setN.retainAll(setM);
+
+        System.out.println(setN.size());
+
+        for (String result : setN) {
+            System.out.println(result);
+        }
+    }
+}
