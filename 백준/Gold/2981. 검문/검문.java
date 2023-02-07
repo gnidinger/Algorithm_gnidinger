d2a04ec30d80fdc809b1b47cd7780bdd59ee90ad
gnidinger <gcnb87@gmail.com>
2023-02-07 12:13:05 +0900

    [Gold IV] Title: 검문, Time: 1508 ms, Memory: 15332 KB -BaekjoonHub

---
+++ 백준/Gold/2981. 검문/검문.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		List<Integer> list = new ArrayList<>();
+
+		for (int i = 0; i < n; i++) {
+			list.add(Integer.parseInt(br.readLine()));
+		}
+
+		Collections.sort(list);
+
+		int result = list.get(1) - list.get(0);
+
+		for (int i = 2; i < n; i++) {
+			result = findGcd(result, list.get(i) - list.get(0));
+		}
+
+		StringBuilder stringBuilder = new StringBuilder();
+
+		for (int i = 2; i <= result; i++) {
+			if (result % i == 0) {
+				stringBuilder.append(i).append(" ");
+			}
+		}
+		System.out.println(stringBuilder.toString());
+	}
+
+	static int findGcd(int a, int b) {
+		while(b != 0) {
+			int r = a % b;
+			a = b;
+			b = r;
+		}
+		return a;
+	}
+}
