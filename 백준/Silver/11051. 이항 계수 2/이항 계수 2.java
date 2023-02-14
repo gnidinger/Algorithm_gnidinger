cfdd37c929468107b66a227ba543504064944ec8
gnidinger <gcnb87@gmail.com>
2023-02-14 22:06:56 +0900

    [Silver II] Title: 이항 계수 2, Time: 228 ms, Memory: 21460 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+	private static int[][] memo;
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int r = sc.nextInt();
+		memo = new int[n + 1][r + 1];
+
+		System.out.println(combination(n, r));
+	}
+
+	static int combination(int n, int r) {
+
+		if (memo[n][r] > 0) {
+			return memo[n][r];
+		}
+
+		if (n == r || r == 0) {
+			return memo[n][r] = 1;
+		}
+
+		memo[n][r] = (combination(n - 1, r) + combination(n - 1, r - 1)) % 10007;
+
+		return memo[n][r];
+	}
+}
