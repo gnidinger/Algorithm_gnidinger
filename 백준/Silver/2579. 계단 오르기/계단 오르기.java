a5102a26c9ef2e8a2f8767a6dbd08ae7bd0ab13d
gnidinger <gcnb87@gmail.com>
2023-03-25 11:15:21 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,36 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    private static int[] memo;
 
-    public static void main(String[] args) {
+	private static int[] memo;
 
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
 
-        int n = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] arr = new int[n + 1];
-        memo = new int[arr.length];
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n + 1];
+		memo = new int[n + 1];
 
-        for (int i = 0; i < n; i++) {
-            arr[i + 1] = sc.nextInt();
-        }
+		for (int i = 0; i < n; i++) {
+			arr[i + 1] = Integer.parseInt(br.readLine());
+		}
 
-        System.out.println(bottomUp(arr, n));
-    }
+		System.out.println(stairs(arr));
 
-    private static int bottomUp(int[] arr, int i) {
+	}
 
-        memo[1] = arr[1];
-        if (i >= 2) {
-            memo[2] = arr[1] + arr[2];
-        }
+	public static int stairs(int[] arr) {
 
-        for (int j = 3; j <= arr.length - 1; j++){
-            memo[j] = arr[j] + Math.max(memo[j - 2], arr[j - 1] + memo[j - 3]);
-        }
+		memo[1] = arr[1];
 
-        return memo[i];
+		if (arr.length >= 3) {
+			memo[2] = arr[1] + arr[2];
+		}
 
-    }
+		for (int i = 3; i < arr.length; i++) {
+			memo[i] = arr[i] + Math.max(memo[i - 2], arr[i - 1] + memo[i - 3]);
+		}
+
+		return memo[arr.length - 1];
+	}
 }
