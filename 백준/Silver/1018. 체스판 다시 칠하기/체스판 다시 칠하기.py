9e074fb994c798db1919b2543d68e25967a165bc
gnidinger <gcnb87@gmail.com>
2023-03-27 13:19:38 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 88 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,29 +1,26 @@
 import sys
 
 n, m = map(int, sys.stdin.readline().split())
-
-board = []
-
-for i in range(n):
-    board.append(list(sys.stdin.readline().strip()))
+board = [list(sys.stdin.readline().strip()) for _ in range(n)]
 
 cnt = []
-
-for i in range(0, n - 7):
-    for j in range(0, m - 7):
+for i in range(n - 7):
+    for j in range(m - 7):
         cntw = 0
         cntb = 0
         for a in range(i, i + 8):
             for b in range(j, j + 8):
                 coor = a + b - i - j
-                if (coor % 2 == 0 and board[a][b] != board[i][j]) or (
-                    coor % 2 == 1 and board[a][b] == board[i][j]
-                ):
-                    cntw += 1
-                if (coor % 2 == 0 and board[a][b] == board[i][j]) or (
-                    coor % 2 == 1 and board[a][b] != board[i][j]
-                ):
-                    cntb += 1
+                if coor % 2 == 0:
+                    if board[a][b] != board[i][j]:
+                        cntw += 1
+                    else:
+                        cntb += 1
+                else:
+                    if board[a][b] == board[i][j]:
+                        cntw += 1
+                    else:
+                        cntb += 1
         cnt.append(cntw)
         cnt.append(cntb)
 
