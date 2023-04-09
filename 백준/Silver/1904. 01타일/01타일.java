7acd55c5c75abf87a89edc4dcb8aa8088b5e9104
gnidinger <gcnb87@gmail.com>
2023-04-09 21:57:49 +0900

    [Silver III] Title: 01타일, Time: 140 ms, Memory: 18068 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -1,27 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
+		int n = Integer.parseInt(br.readLine());
 
-        System.out.println(bottomUp(n));
-    }
+		int[] memo = new int[n + 1];
 
+		memo[0] = 1;
+		memo[1] = 1;
 
-    private static long bottomUp(int n) {
+		for (int i = 2; i <= n; i++) {
+			memo[i] = (memo[i - 1] + memo[i - 2]) % 15746;
+		}
 
-        long[] memo = new long[n + 1];
-        memo[0] = 1;
-        memo[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            memo[i] = (memo[i - 1] + memo[i - 2]) % 15746;
-        }
-
-        return memo[n];
-    }
+		System.out.println(memo[n]);
+	}
 }
