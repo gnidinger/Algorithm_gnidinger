df73062fbdb3857eb8446f3f0a7c2677f71b225b
gnidinger <gcnb87@gmail.com>
2023-04-10 10:27:02 +0900

    [Silver IV] Title: 다음 소수, Time: 1408 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.py
+++ 백준/Silver/4134. 다음 소수/다음 소수.py
@@ -3,18 +3,18 @@ import math
 
 t = int(sys.stdin.readline())
 
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(math.sqrt(n) + 1)):
+        if n % i == 0:
+            return False
+    return True
+
+
 for _ in range(t):
     n = int(sys.stdin.readline())
-    if n <= 1:
-        print(2)
-        continue
-    is_prime = False
-    while not is_prime:
-        is_prime = True
-        for i in range(2, int(math.sqrt(n) + 1)):
-            if n % i == 0:
-                is_prime = False
-                break
-        if not is_prime:
-            n += 1
+    while not is_prime(n):
+        n += 1
     print(n)
