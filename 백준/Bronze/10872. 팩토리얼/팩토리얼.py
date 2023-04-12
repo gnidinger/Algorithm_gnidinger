b1e465e8ffcc76ed65e7ca4f5655d082e7307fbb
gnidinger <gcnb87@gmail.com>
2023-04-12 10:26:06 +0900

    [Bronze V] Title: 팩토리얼, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.py
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -1,6 +1,12 @@
 import sys
-import math
+
+
+def fac(n):
+    if n == 0:
+        return 1
+    return n * fac(n - 1)
+
 
 n = int(sys.stdin.readline().rstrip())
 
-print(math.factorial(n))
+print(fac(n))
