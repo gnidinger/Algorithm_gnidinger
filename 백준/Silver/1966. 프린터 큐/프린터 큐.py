c2de24dcaf6a8d07f2ebadb0c4e7860dbf000ebc
gnidinger <gcnb87@gmail.com>
2023-04-17 18:23:28 +0900

    [Silver III] Title: 프린터 큐, Time: 72 ms, Memory: 34120 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -0,0 +1,28 @@
+from collections import deque
+import sys
+
+t = int(sys.stdin.readline().rstrip())
+
+
+for _ in range(t):
+    n, m = map(int, sys.stdin.readline().rstrip().split())
+    dq = deque(list(i for i in range(n)))
+    tmp = deque(list(map(int, sys.stdin.readline().rsplit())))
+
+    cnt = 0
+
+    while True:
+        maxnum = max(tmp)
+        a = tmp.popleft()
+        b = dq.popleft()
+
+        if a == maxnum:
+            if b == m:
+                cnt += 1
+                print(cnt)
+                break
+            else:
+                cnt += 1
+        else:
+            tmp.append(a)
+            dq.append(b)
