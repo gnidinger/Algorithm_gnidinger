c1d8a1b9f5f2a625d024edee78e2575b18c78d07
gnidinger <gcnb87@gmail.com>
2023-04-19 18:18:17 +0900

    [Silver I] Title: RGB거리, Time: 136 ms, Memory: 14540 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,16 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	private static Set<Integer> set = new HashSet<>();
-	private static int[][] memo;
 
 	public static void main(String[] args) throws IOException {
 
@@ -19,7 +13,7 @@ public class Main {
 		int n = Integer.parseInt(br.readLine());
 
 		int[][] arr = new int[n][3];
-		memo = new int[n][3];
+		int[][] memo = new int[n][3];
 
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -29,31 +23,17 @@ public class Main {
 			arr[i][2] = Integer.parseInt(st.nextToken());
 		}
 
-		set.add(topDown(arr, n - 1, 0));
-		set.add(topDown(arr, n - 1, 1));
-		set.add(topDown(arr, n - 1, 2));
-
-		System.out.println(Collections.min(set));
-
-		// System.out.println(Math.min(topDown(arr, n - 1, 0), Math.min(topDown(arr, n - 1, 1), topDown(arr, n - 1, 2)));
-	}
-
-	private static int topDown(int[][] arr, int i, int j) {
-
 		memo[0][0] = arr[0][0];
 		memo[0][1] = arr[0][1];
 		memo[0][2] = arr[0][2];
 
-		if (memo[i][j] == 0) {
-
-			if (j == 0)
-				memo[i][0] = Math.min(topDown(arr, i - 1, 1), topDown(arr, i - 1, 2)) + arr[i][0];
-			else if (j == 1)
-				memo[i][1] = Math.min(topDown(arr, i - 1, 0), topDown(arr, i - 1, 2)) + arr[i][1];
-			else if (j == 2)
-				memo[i][2] = Math.min(topDown(arr, i - 1, 0), topDown(arr, i - 1, 1)) + arr[i][2];
+		for (int i = 1; i < n; i++) {
+			memo[i][0] = Math.min(memo[i - 1][1], memo[i - 1][2]) + arr[i][0];
+			memo[i][1] = Math.min(memo[i - 1][0], memo[i - 1][2]) + arr[i][1];
+			memo[i][2] = Math.min(memo[i - 1][0], memo[i - 1][1]) + arr[i][2];
 		}
 
-		return memo[i][j];
+		System.out.println(Math.min(memo[n - 1][0], Math.min(memo[n - 1][1], memo[n - 1][2])));
+
 	}
 }
