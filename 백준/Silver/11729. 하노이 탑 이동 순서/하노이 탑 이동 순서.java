32caf7652e3e67177065c6bd1f304f865cc79e07
gnidinger <gcnb87@gmail.com>
2023-04-23 22:00:58 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 496 ms, Memory: 45608 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -1,33 +1,35 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
+	private static final StringBuilder sb = new StringBuilder();
 
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
 
-        int n = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        sb.append((int)Math.pow(2, n) - 1 + "\n"); // 총 횟수
+		int n = Integer.parseInt(br.readLine());
 
-        hanoi(n, 1, 2, 3);
+		sb.append((int)(Math.pow(2, n)) - 1).append('\n');
 
-        System.out.println(sb);
+		hanoi(n, 1, 2, 3);
 
-    }
+		System.out.println(sb);
+	}
 
-    static void hanoi(int n, int start, int mid, int to) {
+	private static void hanoi(int n, int start, int mid, int to) {
 
-        if (n == 1) {
-            sb.append(start + " " + to + "\n");
-            return;
-        }
+		if (n == 1) {
+			sb.append(start).append(" ").append(to).append('\n');
+			return;
+		}
 
-        hanoi(n - 1, start, to, mid);
+		hanoi(n - 1, start, to, mid);
 
-        sb.append(start + " " + to + "\n");
+		sb.append(start).append(" ").append(to).append('\n');
 
-        hanoi(n - 1, mid, start, to);
-    }
+		hanoi(n - 1, mid, start, to);
+	}
 }
