fb3358d77cc6869bff86dd62703aeffd05b6db56
gnidinger <gcnb87@gmail.com>
2023-04-24 11:14:37 +0900

    [Silver I] Title: 정수 삼각형, Time: 304 ms, Memory: 27216 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,55 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	private static int[] memo;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int n = Integer.parseInt(br.readLine());
-
 		int[][] arr = new int[n][n];
-		memo = new int[n];
+		int[][] memo = new int[n][n];
 
 		for (int i = 0; i < n; i++) {
-
 			StringTokenizer st = new StringTokenizer(br.readLine());
-
 			for (int j = 0; j <= i; j++) {
-
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 
-		bottomUp(arr);
+		memo[0][0] = arr[0][0];
 
-		System.out.println(Arrays.stream(memo).max().getAsInt());
-	}
-
-	private static void bottomUp(int[][] arr) {
-
-		memo[0] = arr[0][0];
-		
-		for (int i = 1; i < arr.length; i++) {
-			int[] temp = new int[arr.length];
-			for (int j = 0; j < arr.length; j++) {
-				temp[j] = memo[j];
-			}
+		for (int i = 1; i < n; i++) {
 			for (int j = 0; j <= i; j++) {
 				if (j == 0) {
-					memo[j] = temp[0] + arr[i][0];
+					memo[i][j] = memo[i - 1][j] + arr[i][j];
 				} else if (j == i) {
-					memo[j] = temp[j - 1] + arr[i][i];
+					memo[i][j] = memo[i - 1][j - 1] + arr[i][j];
 				} else {
-					memo[j] = Math.max(temp[j - 1], temp[j]) + arr[i][j];
+					memo[i][j] = Math.max(memo[i - 1][j], memo[i - 1][j - 1]) + arr[i][j];
 				}
 			}
 		}
+
+		int result = memo[n - 1][0];
+
+		for (int i = 1; i < n; i++) {
+			result = Math.max(result, memo[n - 1][i]);
+		}
+
+		System.out.println(result);
 	}
 }
