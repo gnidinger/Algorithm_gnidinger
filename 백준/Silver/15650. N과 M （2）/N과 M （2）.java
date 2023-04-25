04acde33ce45f798596b8e29c68ac5d10e09d133
gnidinger <gcnb87@gmail.com>
2023-04-25 16:01:23 +0900

    [Silver III] Title: N과 M (2), Time: 156 ms, Memory: 15880 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,43 +1,45 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+	static int n;
+	static int m;
+	static int[] arr;
+	static boolean[] isVisited;
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[] arr = new int[n]; // 조합을 만들 배열
-        boolean[] isVisited = new boolean[n]; // 선택했는지 체크하는 배열
+	public static void main(String[] args) throws IOException {
 
-        for (int i = 0; i < n; i++){
-            arr[i] = i + 1;
-        }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        comb(arr, isVisited, 0, n, m); // nCr 경우의 수 출력
-    }
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[m];
+		isVisited = new boolean[n + 1];
 
-    static void comb(int[] arr, boolean[] isVisited, int depth, int n, int r) {
-        if(r == 0) { // base condition
-            print(arr, isVisited);
-            return;
-        }
-        if(depth == n) {
-            return;
-        } else {
-            isVisited[depth] = true;
-            comb(arr, isVisited, depth + 1, n, r - 1); // 재귀함수 호출
+		dfs(1, 0);
 
-            isVisited[depth] = false;
-            comb(arr, isVisited, depth + 1, n, r);
-        }
-    }
+	}
 
-    static void print(int[] arr, boolean[] isVisited) { // 배열 출력을 위한 메서드
-        for(int i = 0; i < arr.length; i++) {
-            if(isVisited[i])
-                System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
+	private static void dfs(int start, int depth) {
+		if (depth == m) {
+			for (int num : arr) {
+				System.out.print(num + " ");
+			}
+			System.out.println();
+			return;
+		}
+
+		for (int i = start; i <= n; i++) {
+			if (!isVisited[i - 1]) {
+				isVisited[i - 1] = true;
+				arr[depth] = i;
+				dfs(i + 1, depth + 1);
+				isVisited[i - 1] = false;
+			}
+		}
+	}
 }
