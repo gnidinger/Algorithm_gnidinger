f28a9c4d51e3d6cfb72b5ef2d1db170681d0b7e2
gnidinger <gcnb87@gmail.com>
2023-04-27 17:27:18 +0900

    [Silver III] Title: N과 M (4), Time: 164 ms, Memory: 18424 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,35 +1,42 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+	static int n;
+	static int m;
+	static int[] arr;
+	static StringBuilder sb = new StringBuilder();
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[] arr = new int[n]; // 조합을 만들 배열
+	public static void main(String[] args) throws IOException {
 
-        for (int i = 0; i < n; i++){
-            arr[i] = i + 1;
-        }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        comb(arr, new int[m], 0, 0, n, m); // n개중 r개를 중복 조합으로 뽑는 경우 출력
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[m];
 
-    }
+		dfs(0, 1);
 
-    static void comb(int[] arr, int[] output, int start, int depth, int n, int r) {
+		System.out.println(sb);
+	}
 
-        if(depth == r) {
-            for(int i = 0; i < output.length; i++) {
-                System.out.print(output[i] + " ");
-            }
-            System.out.println();
-            return;
-        }
-        for(int i = start; i < n; i++)  {
-            // 재귀함수 호출
-            output[depth] = arr[i];
-            comb(arr, output, i, depth + 1 , n, r); // i + 1이 아닌 i 대입, r - 1이 아닌 r 대입
-        }
-    }
+	private static void dfs(int depth, int start) {
+
+		if (depth == m) {
+			for (int num : arr) {
+				sb.append(num).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+
+		for (int i = start; i <= n; i++) {
+			arr[depth] = i;
+			dfs(depth + 1, i);
+		}
+	}
 }
