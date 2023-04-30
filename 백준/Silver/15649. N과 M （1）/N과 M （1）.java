9d3550c83d8a43dc515ba0bce198a3b847a72d08
gnidinger <gcnb87@gmail.com>
2023-04-30 19:17:59 +0900

    [Silver III] Title: N과 M (1), Time: 2156 ms, Memory: 63652 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,44 +1,44 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+	static int n;
+	static int m;
+	static int[] arr;
+	static boolean[] isVisited;
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[] arr = new int[n]; // 순열을 만들 배열
+	public static void main(String[] args) throws IOException {
 
-        for (int i = 0; i < n; i++){
-            arr[i] = i + 1;
-        }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[] output = new int[n]; // 순열을 저장하기 위한 배열
-        boolean[] isVisited = new boolean[n]; // 선택했는지 체크하는 배열
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[m];
+		isVisited = new boolean[n + 1];
 
-        per(arr, output, isVisited, 0, n, m); // 2개를 뽑는 경우(r = 2)
-    }
+		dfs(0);
+	}
 
-    //순서를 지키면서 n 개중에서 r 개를 뽑는 경우
-    static void per(int[] arr, int[] output, boolean[] isVisited, int depth, int n, int r) {
-        if (depth == r) { // base condition
-            print(output, r); //순열 출력을 위한 print 함수
-            return;
-        }
+	private static void dfs(int depth) {
+		if (depth == m) {
+			for (int num : arr) {
+				System.out.print(num + " ");
+			}
+			System.out.println();
+			return;
+		}
 
-        for(int i = 0; i < n; i++) {
-            if (isVisited[i] != true) {
-                isVisited[i] = true;
-                output[depth] = arr[i];
-                per(arr, output, isVisited, depth + 1, n, r); // 재귀함수 호출
-                isVisited[i] = false;
-            }
-        }
-    }
-
-    static void print(int[] arr, int r) { // 배열 출력을 위한 메서드
-        for(int i = 0; i < r; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-    }
+		for (int i = 1; i <= n; i++) {
+			if (!isVisited[i]) {
+				isVisited[i] = true;
+				arr[depth] = i;
+				dfs(depth + 1);
+				isVisited[i] = false;
+			}
+		}
+	}
 }
