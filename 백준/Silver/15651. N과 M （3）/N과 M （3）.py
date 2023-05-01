f19945979ed93a5d6ed47e33c6b3f1d25cb8b065
gnidinger <gcnb87@gmail.com>
2023-05-01 22:17:03 +0900

    [Silver III] Title: N과 M (3), Time: 3992 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,18 +1,17 @@
 import sys
 
 n, m = map(int, sys.stdin.readline().rstrip().split())
-
 lst = [0 for _ in range(m)]
-isVisited = [False] * (n + 1)
+memo = [False for _ in range(n + 1)]
 
 
 def dfs(depth):
+    global result
     if depth == m:
-        for num in lst:
-            print(num, '', end='')
+        for i in lst:
+            print(i, '', end='')
         print()
         return
-
     for i in range(1, n + 1):
         lst[depth] = i
         dfs(depth + 1)
