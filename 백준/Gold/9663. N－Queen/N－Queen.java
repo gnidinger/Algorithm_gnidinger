576b7a7a4da1949cec7b94721861cab17f4d48cd
gnidinger <gcnb87@gmail.com>
2023-05-11 01:45:44 +0900

    [Gold IV] Title: N-Queen, Time: 4640 ms, Memory: 14460 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,54 +1,44 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    private static int n;
-    private static List<Integer> list = new ArrayList<>();
-    private static int cnt = 0;
+	static int n;
+	static int result;
+	static int[] column;
 
-    public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = sc.nextInt();
+		n = Integer.parseInt(br.readLine());
+		column = new int[n];
+		dfs(0);
+		System.out.println(result);
+	}
 
-        for (int i = 0; i < n; i++) {
-            list.add(0);
-        }
+	static void dfs(int row) {
 
-        nQueen(0);
-        System.out.println(cnt);
-    }
+		if (row == n) {
+			result++;
+			return;
+		}
 
-    static void nQueen(int depth) {
+		for (int i = 0; i < n; i++) {
+			column[row] = i;
+			if (isPossible(row)) {
+				dfs(row + 1);
+			}
+		}
+	}
 
-        if (depth == n) {
-            cnt++;
-            return;
-        }
-
-        for (int i = 0; i < n; i++) {
-            list.set(depth, i);
-            if (isSafe(depth)) {
-                nQueen(depth + 1);
-            }
-        }
-    }
-
-    static boolean isSafe(int depth) {
-
-        for (int i = 0; i < depth; i++) {
-
-            if (list.get(depth).equals(list.get(i))) {
-                return false;
-            }
-
-            else if (Math.abs(depth - i) == Math.abs(list.get(depth) - list.get(i))) {
-                return false;
-            }
-        }
-        return true;
-    }
+	static boolean isPossible(int row) {
+		for (int i = 0; i < row; i++) {
+			if (column[i] == column[row] || Math.abs(column[row] - column[i]) == row - i) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
