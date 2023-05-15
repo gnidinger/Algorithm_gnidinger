6de2bbec5cd6c2ff0a3401db6a2939272a27ebf6
gnidinger <gcnb87@gmail.com>
2023-05-15 13:39:51 +0900

    [Gold V] Title: 평범한 배낭, Time: 196 ms, Memory: 53856 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,23 +13,25 @@ public class Main {
 
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
-		int[][] arr = new int[n + 1][2];
+
+		int[] w = new int[n + 1];
+		int[] v = new int[n + 1];
 		int[][] memo = new int[n + 1][k + 1];
 
 		for (int i = 1; i <= n; i++) {
-			StringTokenizer temp = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(temp.nextToken());
-			int b = Integer.parseInt(temp.nextToken());
-			int[] tempArr = {a, b};
-			arr[i] = tempArr;
+
+			st = new StringTokenizer(br.readLine());
+
+			w[i] = Integer.parseInt(st.nextToken());
+			v[i] = Integer.parseInt(st.nextToken());
 		}
 
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= k; j++) {
-				if (arr[i][0] > j) {
+				if (w[i] > j) {
 					memo[i][j] = memo[i - 1][j];
 				} else {
-					memo[i][j] = Math.max(memo[i - 1][j], memo[i - 1][j - arr[i][0]] + arr[i][1]);
+					memo[i][j] = Math.max(memo[i - 1][j], memo[i - 1][j - w[i]] + v[i]);
 				}
 			}
 		}
