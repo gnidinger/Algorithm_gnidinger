f3a5e98f5a9b7245368d67adb0b876ced2d7bfb0
gnidinger <gcnb87@gmail.com>
2023-05-18 20:20:19 +0900

    [Gold IV] Title: 스도쿠, Time: 464 ms, Memory: 18156 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -9,86 +9,70 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+	static int[][] board = new int[9][9];
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
 
-        List<List<Integer>> list = new ArrayList<>(); // 기본 리스트
-        List<List<Integer>> revList = new ArrayList<>(); // Column 리스트
-        List<List<Integer>> sqrList = new ArrayList<>(); // 3 X 3 사각형 리스트
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 0; i < 9; i++) {
-            List<Integer> subList = new ArrayList<>();
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 9; j++) {
-                subList.add(Integer.parseInt(st.nextToken()));
-            }
-            list.add(subList);
-        }
+		for (int i = 0; i < 9; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 9; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
-        for (int i = 0; i < 9; i++) {
-            int finalI = i;
-            List<Integer> colList = list.stream()
-                    .map(a -> a.get(finalI))
-                    .collect(Collectors.toList());
-            revList.add(colList);
-        }
+		sudoku(0, 0);
+	}
 
-        for (int i = 0; i < 9; i = i + 3) {
-            for (int j = 0; j < 9; j = j + 3) {
-                int finalJ = j;
-                List<Integer> subList = list.subList(i, i + 3).stream()
-                        .map(a -> a.subList(finalJ, finalJ + 3))
-                        .flatMap(Collection::stream)
-                        .collect(Collectors.toList());
-                sqrList.add(subList);
-            }
-        }
+	private static boolean sudoku(int row, int col) {
+		if (col == 9) {
+			return sudoku(row + 1, 0);
+		}
 
-        sudoku(list, revList, sqrList, 0, 0);
-    }
+		if (row == 9) {
+			printBoard();
+			return true;
+		}
 
-    private static void sudoku(List<List<Integer>> list, List<List<Integer>> revList, List<List<Integer>> sqrList, int row, int col) {
+		if (board[row][col] == 0) {
+			for (int num = 1; num <= 9; num++) {
+				if (isPossible(row, col, num)) {
+					board[row][col] = num;
+					if (sudoku(row, col + 1)) {
+						return true;
+					}
+					board[row][col] = 0;
+				}
+			}
+			return false;
+		} else {
+			return sudoku(row, col + 1);
+		}
+	}
 
-        if (col > 8) {
-            sudoku(list, revList, sqrList, row + 1, 0);
-            return;
-        }
+	private static boolean isPossible(int row, int col, int num) {
 
-        if (row > 8) {
+		int rowStart = (row / 3) * 3;
+		int colStart = (col / 3) * 3;
 
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    System.out.print(list.get(i).get(j) + " ");
-                }
-                System.out.println();
-            }
-            System.exit(0);
+		for (int i = 0; i < 9; i++) {
+			if (board[row][i] == num || board[i][col] == num || board[rowStart + (i / 3)][colStart + (i % 3)] == num) {
+				return false;
+			}
+		}
 
-        }
+		return true;
+	}
 
-        if (list.get(row).get(col) == 0) {
-            for (int i = 1; i <= 9; i++) {
-                if (isPossible(list, revList, sqrList, row, col, i)) {
-                    list.get(row).set(col, i);
-                    revList.get(col).set(row, i);
-                    sqrList.get((row / 3) * 3 + col / 3).set((row % 3) * 3 + col % 3, i);
-                    sudoku(list, revList, sqrList, row, col + 1);
-                }
-            }
-            list.get(row).set(col, 0);
-            revList.get(col).set(row, 0);
-            sqrList.get((row / 3) * 3 + col / 3).set((row % 3) * 3 + col % 3, 0);
-            return;
-        }
-        sudoku(list, revList, sqrList, row, col + 1);
-    }
-
-    private static boolean isPossible(List<List<Integer>> list, List<List<Integer>> revList, List<List<Integer>> sqrList, int row, int col, int value) {
-
-        if (list.get(row).contains(value)) return false;
-        if (revList.get(col).contains(value)) return false;
-        if (sqrList.get((row / 3) * 3 + col / 3).contains(value)) return false;
-        return true;
-    }
+	private static void printBoard() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				sb.append(board[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
 }
