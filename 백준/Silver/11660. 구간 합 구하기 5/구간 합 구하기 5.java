de4f6553b967edab6fbd8ba0bc9cfb2b664a27e5
gnidinger <gcnb87@gmail.com>
2023-05-25 11:26:31 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 844 ms, Memory: 118608 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[][] table = new int[n + 1][n + 1];
+
+		for (int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= n; j++) {
+				table[i][j] = Integer.parseInt(st.nextToken());
+				table[i][j] += table[i - 1][j] + table[i][j - 1] - table[i - 1][j - 1];
+			}
+		}
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+
+			int sum = table[x2][y2] - table[x1 - 1][y2] - table[x2][y1 - 1] + table[x1 - 1][y1 - 1];
+			sb.append(sum).append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+}
