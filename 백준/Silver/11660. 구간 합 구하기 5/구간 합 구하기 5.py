82a8d468bfa8622060630c5fe441ee1ff3ac71a1
gnidinger <gcnb87@gmail.com>
2023-05-26 12:06:29 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 904 ms, Memory: 81456 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -9,7 +9,10 @@ for i in range(1, n + 1):
     for j in range(1, n + 1):
         table[i][j] += table[i - 1][j] + table[i][j - 1] - table[i - 1][j - 1]
 
+result = []
 for i in range(m):
     x1, y1, x2, y2 = map(int, sys.stdin.readline().rstrip().split())
     sum = table[x2][y2] - table[x1 - 1][y2] - table[x2][y1 - 1] + table[x1 - 1][y1 - 1]
-    print(sum)
+    result.append(str(sum))
+    
+print('\n'.join(result))
