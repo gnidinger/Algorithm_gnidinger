78f36b3dbdcfc6edbce567b31740457888ef27ce
gnidinger <gcnb87@gmail.com>
2023-05-28 12:09:23 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -0,0 +1,21 @@
+import sys
+
+string = sys.stdin.readline().rstrip()
+n = len(string)
+q = int(sys.stdin.readline().rstrip())
+lst = [[0] * (n + 1) for _ in range(26)]
+
+for i, char in enumerate(string):
+    for j in range(26):
+        lst[j][i + 1] = lst[j][i]
+    lst[ord(char) - ord('a')][i + 1] += 1
+
+for _ in range(q):
+    query = list(sys.stdin.readline().rstrip().split())
+    char = ord(query[0]) - ord('a')
+    start = int(query[1])
+    end = int(query[2])
+
+    count = lst[char][end + 1] - lst[char][start]
+
+    print(count)
