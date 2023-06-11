2c071f639cdf3d009b893f2880e8fca56b942268
gnidinger <gcnb87@gmail.com>
2023-06-11 21:47:26 +0900

    [Bronze V] Title: A/B, Time: 148 ms, Memory: 9740 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,10 @@
+const rl = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question('', (line) => {
+    const [a, b] = line.split(' ').map(Number);
+    console.log(a / b);
+    rl.close();
+});
