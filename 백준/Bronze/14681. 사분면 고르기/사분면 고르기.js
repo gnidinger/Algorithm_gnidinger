5e4949b698ff99415d58eef1fe25396f6d1e0952
gnidinger <gcnb87@gmail.com>
2023-06-18 01:09:43 +0900

    [Bronze V] Title: 사분면 고르기, Time: 140 ms, Memory: 9436 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
@@ -0,0 +1,29 @@
+const rl = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', (line) => {
+    input.push(line);
+
+    if (input.length === 2) {
+        const x = parseInt(input[0]);
+        const y = parseInt(input[1]);
+
+        if (x > 0 && y > 0) {
+            console.log(1);
+        } else if (x < 0 && y > 0) {
+            console.log(2);
+        } else if (x < 0 && y < 0) {
+            console.log(3);
+        } else if (x > 0 && y < 0) {
+            console.log(4);
+        }
+
+        rl.close();
+    }
+}).on('close', () => {
+    process.exit(0);
+});
