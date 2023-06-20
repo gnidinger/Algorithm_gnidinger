41dd5f02c26badd673f7900bc9233b1027819c23
gnidinger <gcnb87@gmail.com>
2023-06-20 21:54:42 +0900

    [Bronze V] Title: 구구단, Time: 140 ms, Memory: 9488 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.js
+++ 백준/Bronze/2739. 구구단/구구단.js
@@ -3,18 +3,12 @@ const rl = require('readline').createInterface({
     output: process.stdout,
 });
 
-rl.question('', (line) => {
+rl.on('line', (line) => {
     const a = parseInt(line);
 
-    console.log(a, '*', 1, '=', a * 1);
-    console.log(a, '*', 2, '=', a * 2);
-    console.log(a, '*', 3, '=', a * 3);
-    console.log(a, '*', 4, '=', a * 4);
-    console.log(a, '*', 5, '=', a * 5);
-    console.log(a, '*', 6, '=', a * 6);
-    console.log(a, '*', 7, '=', a * 7);
-    console.log(a, '*', 8, '=', a * 8);
-    console.log(a, '*', 9, '=', a * 9);
+    for (let i = 1; i <= 9; i++) {
+        console.log(`${a} * ${i} = ${a * i}`);
+    }
 
     rl.close();
 });
