2c635f237a330cc17dfcbf5aaf797d867cf63508
gnidinger <gcnb87@gmail.com>
2023-06-21 13:35:44 +0900

    [Bronze V] Title: A+B - 8, Time: 152 ms, Memory: 9692 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,23 @@
+const rl = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let cnt = 0;
+let input = [];
+
+rl.on('line', (line) => {
+    if (cnt === 0) {
+        cnt = parseInt(line);
+    } else {
+        input.push(line.split(' ').map(Number));
+
+        if (input.length === cnt) {
+            for (let i = 0; i < cnt; i++) {
+                const [a, b] = input[i];
+                console.log(`Case #${i + 1}: ${a} + ${b} = ${a + b}`);
+            }
+            rl.close();
+        }
+    }
+});
