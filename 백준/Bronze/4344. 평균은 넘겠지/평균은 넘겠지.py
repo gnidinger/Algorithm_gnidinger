51ca56714268802dbb6139c818dd50f5fb84fae8
거니맘 <gnidinger@gmail.com>
2023-06-27 21:59:02 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -10,15 +10,11 @@ for i in range(c):
     score = total[1:]
     average = sum(score) / number
     count = 0
-
-    # print(number)
-    # print(score)
-    # print(sum(score))
-    # print(average)
-
     for j in score:
         if j > average:
             count += 1
-    # result.append(count / number)
-    rate = count * 100 / number
-    print(f'{rate:.3f}%')
+    result.append(count * 100 / number)
+
+result = [f'{i:.3f}%' for i in result]
+
+print(*result, sep='\n')
