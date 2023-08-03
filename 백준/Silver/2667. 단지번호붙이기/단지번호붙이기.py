4c9409124100215d67bd6ba474dd54ec9d710dd8
거니맘 <gnidinger@gmail.com>
2023-08-03 16:16:32 +0900

    [Silver I] Title: 단지번호붙이기, Time: 44 ms, Memory: 31332 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -3,33 +3,34 @@ import sys
 n = int(sys.stdin.readline().rstrip())
 map = [list(map(int, list(sys.stdin.readline().rstrip()))) for _ in range(n)]
 visited = [[False] * n for _ in range(n)]
+house = 0
 houselst = []
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
 def dfs(x, y):
+    global house
     visited[x][y] = True
-    cnt = 1
+    house += 1
 
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-
         if nx >= 0 and ny >= 0 and nx < n and ny < n:
             if map[nx][ny] == 1 and not visited[nx][ny]:
-                cnt += dfs(nx, ny)
-
-    return cnt
+                dfs(nx, ny)
 
 
 for i in range(n):
     for j in range(n):
         if map[i][j] == 1 and not visited[i][j]:
-            houselst.append(dfs(i, j))
+            house = 0
+            dfs(i, j)
+            houselst.append(house)
 
 houselst = sorted(houselst)
 print(len(houselst))
 
-for house in houselst:
-    print(house)
+for count in houselst:
+    print(count)
