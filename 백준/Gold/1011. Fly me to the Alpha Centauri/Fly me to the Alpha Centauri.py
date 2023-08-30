f19b9de2ab0385723e7a87abc1c85143cfd38920
거니맘 <gnidinger@gmail.com>
2023-08-30 20:19:01 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 356 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
@@ -0,0 +1,18 @@
+import sys
+
+t = int(sys.stdin.readline().rstrip())
+
+for _ in range(t):
+    x, y = map(int, sys.stdin.readline().rstrip().split())
+    d = y - x
+
+    k = 1
+    while k**2 < d:
+        k += 1
+
+    if k**2 == d:
+        print(2 * k - 1)
+    elif d <= k**2 - k:
+        print(2 * k - 2)
+    else:
+        print(2 * k - 1)
