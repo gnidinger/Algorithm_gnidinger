aea62e6b4d6bda2eb49d6ea3a3d06da8f54099d1
거니맘 <gnidinger@gmail.com>
2023-09-23 08:05:43 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -0,0 +1,9 @@
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+
+for i in range(1, N):
+    print("*" * i + " " * 2 * (N - i) + "*" * i)
+
+for i in range(N, 0, -1):
+    print("*" * i + " " * 2 * (N - i) + "*" * i)
